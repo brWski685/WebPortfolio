28eab5a0b1eba34acc259db413c91ce8496b2a34
Elia Maximilian Stamm <97916714+brWski685@users.noreply.github.com>
2024-11-04 15:38:01 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        } else{
+            entry.target.classList.remove("show");
+        }
+    })
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => {observer.observe(el)});
+
+function typeWriter(id, text, speed, index_1, index_2) {
+    if (index_2 < text[index_1].length) {
+        document.getElementById(id).innerHTML += text[index_1][index_2];
+        setTimeout(typeWriter, speed, id, text, speed, index_1, index_2 + 1);
+    }
+    else {
+        if(text[index_1 +1] != null){
+            document.getElementById(id).innerHTML += "<br>";
+            setTimeout(typeWriter, speed, id, text, speed, index_1 + 1, 0);
+        }
+    }
+}
+
+typeWriter("typewriter_animation_1" , ["Hier finden Sie eine Auswahl meiner Projekte.", "Sie spiegeln nur einen kleinen Teil meiner Interessensgebiete wider.", "Oft dienen sie nur dazu, sich mit einer neuen Technologie, einem Algorithmus oder einer Programmiersprache vertraut zu machen."], 15, 0, 0);
